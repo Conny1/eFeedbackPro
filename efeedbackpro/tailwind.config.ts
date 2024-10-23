9d06dfef8bbf9599a2ff3dea493eb6297ead4534
Conrad Joel Mbuya <joelconrad277@gmail.com>
2024-10-23 22:45:52 +0300

    efeedbackPro v-2 setup

--- efeedbackpro/tailwind.config.ts
+++ efeedbackpro/tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import { withUt } from "uploadthing/tw";
 
 const config: Config = {
   content: [
@@ -9,13 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
   plugins: [],
 };
-export default withUt(config);
+export default config;
